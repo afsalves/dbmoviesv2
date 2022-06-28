a250d2b08cae68bca13e7feda4435c8377009fcf
Aline Alves <48930961+afsalves@users.noreply.github.com>
2022-06-27 22:47:31 -0300

    Adding image on horizontal carousel

--- src/components/HorizontalCarousel/index.tsx
+++ src/components/HorizontalCarousel/index.tsx
@@ -10,6 +10,8 @@ type Props = {
 
 export function HorizontalCarousel ({data, title}: Props) {
 
+  const baseUrl = 'https://image.tmdb.org/t/p/original'
+
   return (
     <>
       <Styled.CategorieWrapper>
@@ -19,18 +21,21 @@ export function HorizontalCarousel ({data, title}: Props) {
         horizontal
         style={{ marginTop:20 }}
         showsHorizontalScrollIndicator={false}
+        autoplay
+        autoplayDelay={3}
+        autoplayLoop
         data={data}
         keyExtractor={item => item.id} 
         renderItem={({ item }) => {
           const name = item.original_title
           const rate = item.vote_average
           const id = item.id
+          const img = baseUrl.concat(item.poster_path)
           return (
-            <CardMovie id={id} name={name}rate={rate}/>
+            <CardMovie id={id} name={name}rate={rate} image={img}/>
           )
       }}
       />
-      <CardMovie id={1} name={'Movie'} rate={5.5}/>
     </>
   );
 };
